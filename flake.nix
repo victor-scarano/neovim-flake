05241b11d68751b71fd45ecfe38431a9e57b2175
Victor Scarano <victorascarano@gmail.com>
2025-05-11 21:31:25 -0500

    trying to expose home manager module

--- flake.nix
+++ flake.nix
@@ -83,7 +83,7 @@
 						pkgs.yazi
 
 						# TODO: these need to be optional
-						(lib.optional config.my-neovim.languages.rust.enable pkgs.rust-analyzer)
+						# (lib.optional config.my-neovim.languages.rust.enable pkgs.rust-analyzer)
 						# clang-tools
 						# lua-language-server
 						# nixd
@@ -92,7 +92,7 @@
 						# taplo
 						# vscode-langservers-extracted
 						# zls
-					];
+					] ++ (lib.optional config.my-neovim.languages.rust.enable pkgs.rust-analyzer);
 				};
 			};
 		};
